d69ca8a660d3f2b8300f061cfb32afa02f202c16
jackeyyao <jackey@workstation.local>
2015-11-27 17:41:51 +0800

    create code base

---
+++ EReader/EReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EReader
+//
+//  Created by jackey on 15/11/27.
+//  Copyright © 2015年 jackey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
